f6bd6942b077389593ed89a91a3fa5014cc2995e
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-06 11:19:13 +0100

    complete scaffolding

---
+++ src/components/AppProva.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-5">{{this.prova}} </div>
+    <div class="green">style variable</div>
+</template>
+
+<script>
+    export default{
+        name:'AppProva',
+        data() {
+            return {
+                prova: 'componente prova'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/views/ProvaView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mt-5">prova view</div>
+</template>
+
+<script>
+export default{
+    name: 'ProvaView',
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
