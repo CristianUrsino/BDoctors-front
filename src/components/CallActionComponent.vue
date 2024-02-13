f9f8ba2e6162e1b4870cf1b9639536c01b98e6ba
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-02-13 18:59:02 +0100

    initial call to action update

--- src/components/CallActionComponent.vue
+++ src/components/CallActionComponent.vue
@@ -1,53 +1,89 @@
 <template>
-    <div class="call-action">
-      <div class="container">
-        <div class="text-center text-white ms-5">
-            <h1 class="ms-5">Sei un medico? Registrati!</h1>
-        </div>
-        <div class="d-flex">
-            <div class="border-rounded">
-                <img src="/images/bdoctors1.jpg" alt="">
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex call-action">
+          <div class="col-2 d-flex justify-content-center">
+            <div class="call-action-img border-rounded my-5">
+              <img src="/images/bdoctors1.jpg" alt="" />
             </div>
-            
-            <div class="mt-4 ms-5">
-                <p class="fs-2 text-white">Iscriviti subito al nostro sito e inizia a lavorare come medico</p>
-                <div class="text-center pt-5">
-                    <button class="btn btn-primary fs-2">Iscriviti</button>
-                </div>
+          </div>
+          <div class="col-10 call-action-text text-white my-4">
+            <h1 class="">Sei un medico? Registrati!</h1>
+            <ul>
+              <li class="d-flex gap-2">
+                <i class="fa-solid fa-share mt-2"></i>
+                <p class="">
+                  Iscriviti subito al nostro sito e inizia a lavorare come
+                  medico.
+                </p>
+              </li>
+              <li class="d-flex gap-2">
+                <i class="fa-solid fa-share mt-2"></i>
+                <p class="">
+                  Controlla le tue statistiche e le tue recensioni.
+                </p>
+              </li>
+            </ul>
+            <div class="text-center pt-5">
+              <button class="btn btn-primary">Iscriviti</button>
             </div>
+          </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'CallActionComponent',
-        data(){
-            return{
-                
-            }
-        },
-
-    }
+export default {
+  name: "CallActionComponent",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.call-action{
-    background-color: #74C0FC;
-    padding-top: 100px;
-    padding-bottom: 100px;
+@import "@/assets/styles/partials/_variables.scss";
+.call-action-img {
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
-.border-rounded{
-    border-radius: 50px;
-    overflow: hidden;
+.call-action-text{
+    padding:0 100px;
 }
-img{
-    width:100%;
-    transition: transform 0.3s, filter 0.3s ease-in-out;
+.call-action {
+  background-color: $blue-green;
+
+  margin-top: 100px;
+  margin-bottom: 100px;
+  border-radius: 20px;
+  border: 1px solid rgb(190, 190, 190);
+  box-shadow: 5px 5px 5px lightblue;
+  ul{
+    padding:0;
+    p{
+        margin:0
+    }
+  }
+}
+.border-rounded {
+  border-radius: 50px;
+  overflow: hidden;
+}
+img {
+  width: 100%;
+  transition: transform 0.3s, filter 0.3s ease-in-out;
 }
 .border-rounded:hover img {
-  transform: scale(1.1); 
+  transform: scale(1.1);
   filter: brightness(1.2);
 }
 </style>
