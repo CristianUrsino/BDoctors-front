fe4ac89572529a6c847bf90b398a112b04ab2adb
burillu <burillu90@hotmail.it>
2024-02-19 18:28:52 +0100

    only sponsored doctors in featured

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,68 @@
 <template>
-  <AppNav/>
-  
-  
-  <div>
-    <router-view></router-view>
+  <div v-if="store.loading">
+    <div class="vh-100 my-bg-primary">
+      caricamento...
+    </div>
   </div>
-  
-  <AppFooter />
+  <div v-else>
+    <AppNav />
+
+
+    <div>
+      <router-view></router-view>
+    </div>
+
+    <AppFooter />
+
+  </div>
 </template>
 
 <script>
+import { store } from './store';
+import axios, { Axios } from "axios";
 import AppProva from './components/AppProva.vue';
 import AppFooter from './components/AppFooter.vue';
 import Home from './views/Home.vue';
 import AppNav from './components/AppNav.vue';
 import DoctorDetails from './views/DoctorDetails.vue';
 
-export default{
-        name:'App',
-        data() {
-            return {
-              
-            }
-        },
-        components:{
-          AppProva,
-          AppFooter,
-          Home,
-          AppNav,
-          DoctorDetails
-        }
+export default {
+  name: 'App',
+  data() {
+    return {
+      store,
+
     }
+  },
+  components: {
+    AppProva,
+    AppFooter,
+    Home,
+    AppNav,
+    DoctorDetails
+  },
+  methods: {
+    getAllDoctors() {
+      this.store.loading = true;
+      axios.get(store.apiBaseUrl + "/doctors").then((res) => {
+        console.log(res.data.results);
+        this.store.doctors = res.data.results;
+        // this.sponsoredDoctors = this.doctorList.slice(0, 3);
+      }).catch(err => {
+        console.log(err);
+      }).finally(el => {
+        this.store.loading = false;
+      });
+    },
+  },
+  created() {
+    this.getAllDoctors();
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  
-.red{
-  color:red;
+.red {
+  color: red;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
-import axios, { Axios } from "axios";
+
 import { reactive } from "vue";
 
 export const store = reactive({
     apiBaseUrl: "http://127.0.0.1:8000/api",
     imagesBaseUrl: "http://127.0.0.1:8000/storage/",
     selectedProfession: '',
-    
+    doctors: null,
+    loading: false,
+
 });
