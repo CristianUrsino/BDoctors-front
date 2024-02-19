6b3bd5b79e54fcc72a9a5b76dc02a6c03eec6ac4
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-02-19 12:05:57 +0100

    bug router-link fixed

--- src/views/DoctorSearch.vue
+++ src/views/DoctorSearch.vue
@@ -38,8 +38,6 @@
                         <router-link class="btn detail-btn text-uppercase"
                             :to="{ name: 'DoctorDetail', params: {slug: doctor.slug} }"> Mostra dettagli
                             </router-link>
-                                :to="{ name: 'DoctorDetail', params: {slug: doctor.slug} }"> Mostra dettagli
-                                </router-link>
                         <v-btn variant="text" color="teal-accent-4" @click="revealDoctor(doctor)">
                             Contatti
                         </v-btn>
